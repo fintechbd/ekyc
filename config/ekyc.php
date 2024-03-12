5db83bf8423cea20d7e0e627ec231cb7ae2de814
Mohammad Hafijul Islam <hafijul233@gmail.com>
2024-03-13 02:13:31 +0600

    kyc env value updated

--- config/ekyc.php
+++ config/ekyc.php
@@ -9,7 +9,7 @@ return [
     |--------------------------------------------------------------------------
     | This setting enable the API will be available or not
     */
-    'enabled' => env('PACKAGE_Ekyc_ENABLED', true),
+    'enabled' => env('PACKAGE_EKYC_ENABLED', true),
 
     /*
     |--------------------------------------------------------------------------
