61bc5ee6963574a1898cedfc380a761cdf4d88b2
hafijul233 <hafijul233@users.noreply.github.com>
2024-05-07 21:04:44 +0000

    Fix styling

--- src/EkycServiceProvider.php
+++ src/EkycServiceProvider.php
@@ -53,7 +53,7 @@ class EkycServiceProvider extends ServiceProvider
 
         if ($this->app->runningInConsole()) {
             $this->commands([
-                InstallCommand::class
+                InstallCommand::class,
             ]);
         }
     }
