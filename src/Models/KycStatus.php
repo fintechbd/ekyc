6e45c064dc3ea981beb3287ec0a18381012391f2
Mohammad Hafijul Islam <hafijul233@gmail.com>
2024-05-09 12:45:29 +0600

    core message lang file moved to rest api

--- src/Models/KycStatus.php
+++ src/Models/KycStatus.php
@@ -59,10 +59,10 @@ class KycStatus extends BaseModel
         $primaryKey = $this->getKey();
 
         $links = [
-            'show' => action_link(route('ekyc.kyc-statuses.show', $primaryKey), __('core::messages.action.show'), 'get'),
-            'update' => action_link(route('ekyc.kyc-statuses.update', $primaryKey), __('core::messages.action.update'), 'put'),
-            'destroy' => action_link(route('ekyc.kyc-statuses.destroy', $primaryKey), __('core::messages.action.destroy'), 'delete'),
-            'restore' => action_link(route('ekyc.kyc-statuses.restore', $primaryKey), __('core::messages.action.restore'), 'post'),
+            'show' => action_link(route('ekyc.kyc-statuses.show', $primaryKey), __('restapi::messages.action.show'), 'get'),
+            'update' => action_link(route('ekyc.kyc-statuses.update', $primaryKey), __('restapi::messages.action.update'), 'put'),
+            'destroy' => action_link(route('ekyc.kyc-statuses.destroy', $primaryKey), __('restapi::messages.action.destroy'), 'delete'),
+            'restore' => action_link(route('ekyc.kyc-statuses.restore', $primaryKey), __('restapi::messages.action.restore'), 'post'),
         ];
 
         if ($this->getAttribute('deleted_at') == null) {
