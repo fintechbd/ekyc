334c91fb9337d85c6c425f59b059583a31e2bb42
Mohammad Hafijul Islam <hafijul233@gmail.com>
2024-08-12 00:46:13 +0600

    Append links attribute removed

--- src/Models/KycStatus.php
+++ src/Models/KycStatus.php
@@ -21,7 +21,7 @@ class KycStatus extends BaseModel
 
     protected $guarded = ['id'];
 
-    protected $appends = ['links'];
+
 
     protected $casts = ['kyc_status_data' => 'array', 'restored_at' => 'datetime', 'enabled' => 'bool', 'request' => 'array', 'response' => 'array'];
 
