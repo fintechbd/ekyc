3c650e2f2b509316681c7065b5915fcab312cb2c
Mohammad Hafijul Islam <hafijul233@gmail.com>
2024-08-31 15:47:54 +0600

    project setup updated

--- src/Commands/InstallCommand.php
+++ src/Commands/InstallCommand.php
@@ -2,18 +2,41 @@
 
 namespace Fintech\Ekyc\Commands;
 
+use Fintech\Core\Traits\HasCoreSettingTrait;
 use Illuminate\Console\Command;
 
 class InstallCommand extends Command
 {
+    use HasCoreSettingTrait;
+
     public $signature = 'ekyc:install';
 
-    public $description = 'My command';
+    public $description = 'Configure the system for the `fintech/ekyc` module';
+
+    private array $settings = [
+        [
+            'package' => 'ekyc',
+            'label' => 'KYC Reference Token Count',
+            'description' => 'The last token count value is assigned and the next will be increment by 1.',
+            'key' => 'reference_count',
+            'type' => 'integer',
+            'value' => '1'
+        ]
+    ];
 
     public function handle(): int
     {
-        $this->comment('All done');
+        try {
 
-        return self::SUCCESS;
+            $this->addOverwriteSetting();
+
+            return self::SUCCESS;
+
+        } catch (\Exception $e) {
+
+            $this->components->twoColumnDetail($e->getMessage(), '<fg=red;options=bold>ERROR</>');
+
+            return self::FAILURE;
+        }
     }
 }
