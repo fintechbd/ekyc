77eb9949414b353409a2bdb7ecaed20b329fea97
Mohammad Hafijul Islam <hafijul233@gmail.com>
2024-10-17 15:43:34 +0600

    break: meta data module audit tweak added

--- src/Models/KycStatus.php
+++ src/Models/KycStatus.php
@@ -4,11 +4,14 @@ namespace Fintech\Ekyc\Models;
 
 use Fintech\Core\Abstracts\BaseModel;
 use Fintech\Core\Traits\AuditableTrait;
+use Fintech\Core\Traits\BlameableTrait;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use OwenIt\Auditing\Contracts\Auditable;
 
-class KycStatus extends BaseModel
+class KycStatus extends BaseModel implements Auditable
 {
-    use AuditableTrait;
+    use \OwenIt\Auditing\Auditable;
+    use BlameableTrait;
     use SoftDeletes;
 
     /*
