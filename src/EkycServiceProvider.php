c4c9c22f6c830857e47e42e48e663cc6233e36cd
Mohammad Hafijul Islam <hafijul233@gmail.com>
2025-02-09 18:41:05 +0600

    config tage in publish added

--- src/EkycServiceProvider.php
+++ src/EkycServiceProvider.php
@@ -36,7 +36,7 @@ class EkycServiceProvider extends ServiceProvider
 
         $this->publishes([
             __DIR__.'/../config/ekyc.php' => config_path('fintech/ekyc.php'),
-        ]);
+        ], 'fintech-ekyc-config');
 
         $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
 
@@ -44,7 +44,7 @@ class EkycServiceProvider extends ServiceProvider
 
         $this->publishes([
             __DIR__.'/../lang' => $this->app->langPath('vendor/ekyc'),
-        ]);
+        ], 'fintech-ekyc-lang');
 
         $this->loadViewsFrom(__DIR__.'/../resources/views', 'ekyc');
 
@@ -52,7 +52,7 @@ class EkycServiceProvider extends ServiceProvider
 
         $this->publishes([
             __DIR__.'/../resources/views' => resource_path('views/vendor/ekyc'),
-        ]);
+        ], 'fintech-ekyc-views');
 
         if ($this->app->runningInConsole()) {
             $this->commands([
